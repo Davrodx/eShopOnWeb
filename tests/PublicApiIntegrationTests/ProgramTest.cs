639854f5ec5b1b1c9dd37b1f6361a95e5cd9e1b1
Davgodx <103457564+Davrodx@users.noreply.github.com>
2025-11-06 13:37:16 -0600

    Update ProgramTest.cs

--- tests/PublicApiIntegrationTests/ProgramTest.cs
+++ tests/PublicApiIntegrationTests/ProgramTest.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
 using System.Net.Http;
 
 namespace PublicApiIntegrationTests;
@@ -7,20 +10,31 @@ namespace PublicApiIntegrationTests;
 [TestClass]
 public class ProgramTest
 {
-    private static WebApplicationFactory<Program> _application = new();
-
-    public static HttpClient NewClient
-    {
-        get
-        {
-            return _application.CreateClient();
-        }
-    }
+    private static WebApplicationFactory<Program> _application = null!;
+    public static HttpClient NewClient => _application.CreateClient();
 
     [AssemblyInitialize]
     public static void AssemblyInitialize(TestContext _)
     {
-        _application = new WebApplicationFactory<Program>();
+        _application = new WebApplicationFactory<Program>()
+            .WithWebHostBuilder(builder =>
+            {
+                // Force the "Testing" environment
+                builder.UseEnvironment("Testing");
 
+                // Override service registrations (optional but safest)
+                builder.ConfigureServices(services =>
+                {
+                    // Remove the real DB context
+                    var descriptor = services.SingleOrDefault(
+                        d => d.ServiceType == typeof(DbContextOptions<AppDbContext>));
+                    if (descriptor != null)
+                        services.Remove(descriptor);
+
+                    // Add InMemory DB for testing
+                    services.AddDbContext<AppDbContext>(options =>
+                        options.UseInMemoryDatabase("IntegrationTests"));
+                });
+            });
     }
 }
